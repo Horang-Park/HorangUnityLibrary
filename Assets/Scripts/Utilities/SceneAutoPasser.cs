c2ee7d6820ebf380bdd17f35584a941712d90fdb
HorangPark <horang_park@naver.com>
2022-06-20 15:50:53 +0900

    씬오토패서에 씬 이동 시, 액션을 넣을 수 있도록 수정

--- Assets/Scripts/Utilities/SceneAutoPasser.cs
+++ Assets/Scripts/Utilities/SceneAutoPasser.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using UniRx;
 using UnityEngine;
+using UnityEngine.Events;
 using Utilities;
 using Logger = Utilities.Logger;
 
@@ -8,12 +9,15 @@ public class SceneAutoPasser : MonoBehaviour
 {
 	public string nextSceneName;
 	public float delayTime;
+	public UnityEvent OnMoveNextScene;
 
 	private void Awake()
 	{
 		Observable.FromCoroutine(MoveToNextScene)
 			.DoOnCompleted(() =>
 			{
+				OnMoveNextScene?.Invoke();
+				
 				UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(nextSceneName);
 				
 				Logger.Log(LogPriority.Verbose, $"{nextSceneName} 씬으로 이동합니다.");

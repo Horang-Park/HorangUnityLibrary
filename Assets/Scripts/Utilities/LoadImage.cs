a7978aadf7b83688bec40ed0bf48e286262adf65
HorangPark <horang_park@naver.com>
2022-06-22 17:58:37 +0900

    주석 추가

--- Assets/Scripts/Utilities/LoadImage.cs
+++ Assets/Scripts/Utilities/LoadImage.cs
@@ -8,6 +8,12 @@ namespace Utilities
 {
 	public static class LoadImage
 	{
+		/// <summary>
+		/// 특정 경로에서 이미지 로드 (비용 큼)
+		/// </summary>
+		/// <param name="path">로드 할 경로</param>
+		/// <returns>로드한 이미지 파일을 스프라이트로 반환</returns>
+		/// <exception cref="FileNotFoundException">파일을 찾지 못 함</exception>
 		public static async UniTask<Sprite> LoadFromSpecificPath(string path)
 		{
 			var fileInfo = new FileInfo(path);
@@ -35,6 +41,12 @@ namespace Utilities
 			return GetSpriteFromByteBuffer(fileReadBuffer);
 		}
 
+		/// <summary>
+		/// Resources 폴더에서 이미지 로드
+		/// </summary>
+		/// <param name="path">로드 경로 (상대 경로, 확장자 제외)</param>
+		/// <returns>로드한 스프라이트</returns>
+		/// <exception cref="FileLoadException">이미지가 없거나, 로드하려는 파일이 스프라이가 아닐 경우</exception>
 		public static async UniTask<Sprite> LoadFromResources(string path)
 		{
 			var loadObject = await Resources.LoadAsync<Sprite>(path);
@@ -50,6 +62,11 @@ namespace Utilities
 
 		}
 
+		/// <summary>
+		/// Resources 폴더에서 여러 이미지 로드
+		/// </summary>
+		/// <param name="paths">이미지를 로드할 경로들 (상대 경로, 확장자 제외)</param>
+		/// <returns>로드한 스프라이트 묶음</returns>
 		public static async UniTask<List<Sprite>> LoadManyFromResources(IEnumerable<string> paths)
 		{
 			var tasks = Enumerable.Select(paths, LoadFromResources);

6e83687f5040a5e1c93dbec2ff578b538180e91d
HorangPark <horang_park@naver.com>
2022-07-01 14:45:00 +0900

    패널 코드 수정

--- Assets/Scripts/Utilities/Debug/Panel.cs
+++ Assets/Scripts/Utilities/Debug/Panel.cs
@@ -4,9 +4,47 @@ namespace Utilities.Debug
 {
 	public class Panel : MonoBehaviour
 	{
-#if DEBUG_MODE_PANEL
 		private float deltaTime;
+		
+#if DEBUG_MODE_PANEL // Working on Unity Editor only
+		private void Update()
+		{
+			deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+		}
 
+		private void OnGUI()
+		{
+			var width = Screen.width;
+			var height = Screen.height;
+			var guiStyle = new GUIStyle();
+			var rect = new Rect(0.0f, 0.0f, width, height * 0.02f);
+
+			guiStyle.alignment = TextAnchor.UpperLeft;
+			guiStyle.fontSize = height * 2 / 30;
+			guiStyle.normal.textColor = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+
+			var deltaMillisecond = deltaTime * 1000.0f;
+			var frame = 1.0f / deltaTime;
+			var text = $"{deltaMillisecond:0.0}ms → {frame:0}fps";
+			
+			GUI.Label(rect, text, guiStyle);
+		}
+#endif
+		
+#if !UNITY_EDITOR
+		private void Awake()
+		{
+			var panelGameObject = GameObject.Find("Debug Panel");
+			
+			if (panelGameObject is null)
+			{
+				panelGameObject = new GameObject("Debug Panel");
+				panelGameObject.AddComponent<Panel>();
+				panelGameObject.AddComponent<HardDontDestroyOnLoad>();
+				panelGameObject.gameObject.hideFlags = HideFlags.NotEditable;
+			}
+		}
+		
 		private void Update()
 		{
 			deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
